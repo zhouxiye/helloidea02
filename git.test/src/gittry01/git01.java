3ca7f7e8bda41d41f486640f835e49fd247e63bb
uiei <uiei@atuiei.com>
2023-01-12 10:55:40 +0800

    second commit

--- git.test/src/gittry01/git01.java
+++ git.test/src/gittry01/git01.java
@@ -3,5 +3,6 @@ package gittry01;
 public class git01 {
     public static void main(String[] args) {
         System.out.println("hello,java");
+        System.out.println("hello,java02");
     }
 }
