f66c61fa9a99bb71070b9c9d6e9409b0bc088ecb
uiei <uiei@atuiei.com>
2023-01-13 09:19:54 +0800

    three commit

--- git.test/src/gittry01/git01.java
+++ git.test/src/gittry01/git01.java
@@ -4,5 +4,6 @@ public class git01 {
     public static void main(String[] args) {
         System.out.println("hello,java");
         System.out.println("hello,java02");
+        System.out.println("333333333333333333");
     }
 }
